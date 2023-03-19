9e2a9c5401827ea575887803fc5cb694b006e3f7
Nickls5 <nik.losev.2005@bk.ru>
2023-03-19 18:04:04 +0300

    new code-style

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-> 
->using namespace std;
->
-> int main()
-> {
->   string name;
->   std::cout<<"Enter your name";
->   std::cin>> name;
->   std::cout << "Hello world from" << name << '!';
->   return 0;
-> }
+>> using namespace std;
+>> int
+   main() >
+{
+  > string name;
+  > std::cout << "Enter your name";
+  > std::cin >> name;
+  > std::cout << "Hello world from" << name << '!';
+  > return 0;
+  >
+}
